8af6250777fa9ab25c704691f63699625c97c041
WalterTNFC <walter.tad@hotmail.com>
2023-01-31 23:47:20 -0300

    Verify Req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,4 @@
+from typing import Counter
 from tech_news.database import find_news
 
 
@@ -35,114 +36,18 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    search = find_news()
 
+    # Para ordenar e verificar mais recorrente
+    categories = [result["category"] for result in search]
+    sorted_categories = sorted(categories)
+    count = Counter(sorted_categories)
+    verify_most_common = count.most_common()
 
-NEWS = [
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_0.htm",
-        "title": "noticia_0",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_0",
-        "comments_count": 2,
-        "summary": "Sumario da noticia_0",
-        "category": "Categoria_0",
-        "tags": ["PC_0", "Console_0"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia-bacana",
-        "title": "Notícia bacana",
-        "writer": "Eu",
-        "summary": "Algo muito bacana aconteceu",
-        "comments_count": 4,
-        "timestamp": "04/04/2021",
-        "tags": ["Tecnologia", "Esportes"],
-        "category": "Ferramentas",
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia-legal",
-        "title": "Notícia bacana 2",
-        "writer": "Você",
-        "summary": "Algo muito bacana aconteceu de novo",
-        "comments_count": 1,
-        "timestamp": "07/04/2022",
-        "tags": ["Tecnologia"],
-        "category": "Novidades",
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_3.htm",
-        "title": "noticia_3",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_3",
-        "comments_count": 1,
-        "summary": "Sumario da noticia_3",
-        "category": "Ferramentas",
-        "tags": ["PC_3", "Console_3"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_4.htm",
-        "title": "noticia_4",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_4",
-        "comments_count": 1,
-        "summary": "Sumario da noticia_4",
-        "category": "Ferramentas",
-        "tags": ["PC_4", "Console_4"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_5.htm",
-        "title": "noticia_5",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_5",
-        "comments_count": 1,
-        "summary": "Sumario da noticia_5",
-        "category": "Categoria_0",
-        "tags": ["PC_5", "Console_5"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_6.htm",
-        "title": "noticia_6",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_6",
-        "comments_count": 1,
-        "summary": "Sumario da noticia_6",
-        "category": "Novidades",
-        "tags": ["PC_6", "Console_6"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_7.htm",
-        "title": "noticia_7",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_7",
-        "comments_count": 7,
-        "summary": "Sumario da noticia_1",
-        "category": "Categoria_7",
-        "tags": ["PC_7", "Console_7"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_8.htm",
-        "title": "noticia_8",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_7",
-        "comments_count": 8,
-        "summary": "Sumario da noticia_8",
-        "category": "Categoria_7",
-        "tags": ["PC_7", "Console_6"],
-    },
-    {
-        "url": "https://blog.betrybe.com/novidades/noticia_9.htm",
-        "title": "noticia_9",
-        "timestamp": "23/11/2020",
-        "writer": "Escritor_7",
-        "comments_count": 5,
-        "summary": "Sumario da noticia_9",
-        "category": "Categoria_9",
-        "tags": ["PC_7", "Console_6"],
-    },
-]
+    top_5 = [category[0] for category in verify_most_common]
 
+    return top_5
 
-# top_5_news(NEWS)
 
 # Referências:
 # 1- https://diegomariano.com/como-ordenar-um-dicionario-em-python/
